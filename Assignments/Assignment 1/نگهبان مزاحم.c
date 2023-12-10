dfc3fdbe1200b082f894b950565d6478529d1bdc
Imanm02 <imanm1381@gmail.com>
2023-12-10 18:45:29 +0330

    Update Assignment 1 - clean code

--- Assignments/Assignment 1/نگهبان مزاحم.c
+++ Assignments/Assignment 1/نگهبان مزاحم.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    int a,b,c,d,e,f,g,h,i,determinan;
-    
-    scanf("%d%d%d", &a,&b,&c);
-    scanf("%d%d%d", &d,&e,&f);
-    scanf("%d%d%d", &g,&h,&i);
+    int a, b, c, d, e, f, g, h, i, determinant;
 
-    determinan=a*e*i+b*f*g+c*d*h+(~c+1)*e*g+(~a+1)*f*h+(~b+1)*d*i;
+    scanf("%d%d%d", &a, &b, &c);
+    scanf("%d%d%d", &d, &e, &f);
+    scanf("%d%d%d", &g, &h, &i);
 
-    printf("%d",determinan);
+    determinant = a * e * i + b * f * g + c * d * h + (~c + 1) * e * g + (~a+1) * f * h + (~b + 1) * d * i;
+
+    printf("%d", determinant);
 
     return 0;
 }
