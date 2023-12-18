de6118f1c848ca4ffc4c0cfa6387d74afd5a6a4b
Imanm02 <imanm1381@gmail.com>
2023-12-18 15:33:11 +0330

    Clean code Assignment3 codes

--- Assignments/Assignment 3/Codes/کمی هم گوگل کنیم! - 2.cpp
+++ Assignments/Assignment 3/Codes/کمی هم گوگل کنیم! - 2.cpp
@@ -2,68 +2,64 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-#define calc(a, b, c) ((a) c (b))
+#define calc(a, b, operation) ((a) operation (b))
 
-long long int maximum(long long int a, long long int b, long long int c)
-{
-    if (a >= b && a >= c) {return a;}
-    else if (b >= a && b >= c) {return b;}
-    else {return c;}
+long long int findMaximum(long long int num1, long long int num2, long long int num3) {
+    if (num1 >= num2 && num1 >= num3) return num1;
+    return (num2 >= num3) ? num2 : num3;
 }
-long long int minimum(long long int a, long long int b, long long int c)
-{
-    if (a <= b && a <= c) {return a;}
-    else if (b <= a && b <= c) {return b;}
-    else {return c;}
+
+long long int findMinimum(long long int num1, long long int num2, long long int num3) {
+    if (num1 <= num2 && num1 <= num3) return num1;
+    return (num2 <= num3) ? num2 : num3;
 }
-long long int majmomagh(long long int n) {
-    long long int p=0;
-    if (n==0) {return 0;}
-    else if (n==1) {return 1;}
-    for (long long int i=1; i<=n; i++){
-        if (n%i==0) {
-                p += i;
+
+long long int sumOfDivisors(long long int number) {
+    long long int sum = 0;
+    for (long long int i = 1; i <= number; i++) {
+        if (number % i == 0) sum += i;
+    }
+    return sum;
+}
+
+long long int sumOfProperDivisors(long long int number) {
+    long long int sum = 0;
+    for (long long int i = 1; i * i <= number; i++) {
+        if (number % i == 0) {
+            sum += i;
+            if (i * i != number) sum += number / i;
         }
     }
-    return p;
+    return sum;
 }
-long long int majmomaghh(long long int n) {
-    long long int p=0;
-    if (n==0) {return 0;}
-    else if (n==1) {return 1;}
-    for (long long int i=1; i*i<=n; i++){
-        if (n%i==0) {
-                p += i;
-                p += n/i;
-        }
-    }
-    return p;
-}
+
 #if s == 0
-long long int findDivisorSum(char d, int a, int b, int c){
-        if(d == 'M'){
-            return maximum(majmomaghh(a), majmomaghh(b), majmomaghh(c));
-        }
-        else if(d == 'm'){
-            return minimum(majmomaghh(a), majmomaghh(b), majmomaghh(c));
-        }
+long long int findDivisorSum(char type, int num1, int num2, int num3) {
+    if (type == 'M') {
+        return findMaximum(sumOfProperDivisors(num1), sumOfProperDivisors(num2), sumOfProperDivisors(num3));
+    } else if (type == 'm') {
+        return findMinimum(sumOfProperDivisors(num1), sumOfProperDivisors(num2), sumOfProperDivisors(num3));
     }
+    return 0;
+}
+
 #else
-long long int findDivisorSum(int a, ...){
-        va_list arg;
-        long long int javabema;
-        va_start (arg, a);
-        javabema = majmomagh(va_arg(arg, long long int));
-        for(int i = 1; i < a ; i++){
-            long long int b = majmomagh(va_arg(arg, long long int));
-            if (javabema>b) {javabema = b;}}
-        va_end(arg);
-        return javabema;}
+long long int findDivisorSum(int count, ...) {
+    va_list args;
+    va_start(args, count);
+    long long int minSum = sumOfDivisors(va_arg(args, long long int));
+    for (int i = 1; i < count; i++) {
+        long long int currentSum = sumOfDivisors(va_arg(args, long long int));
+        if (currentSum < minSum) minSum = currentSum;
+    }
+    va_end(args);
+    return minSum;
+}
 #endif
 
 long long run(char type, int num1, int num2, int num3, int num4) {
     return findDivisorSum(type,
-    calc(num1, num2, +),
-    calc(num1, num2, -),
-    calc(num3, num4, *));
+                          calc(num1, num2, +),
+                          calc(num1, num2, -),
+                          calc(num3, num4, *));
 }
