4d4beb38eb899d9a88c9247b748b48e89264b474
Imanm02 <imanm1381@gmail.com>
2023-12-18 14:42:14 +0330

    Clean code Assignment2 Question4

--- Assignments/Assignment 2/Codes/جهان های موازی.c
+++ Assignments/Assignment 2/Codes/جهان های موازی.c
@@ -4,32 +4,31 @@
 int main()
 {
     long long int a, b, c, d, g, e, t;
+
     scanf("%lld%lld", &a, &b);
     scanf("%lld%lld", &c, &d);
-    if (a*c>b*d)
-    {
-    g=a*c;
+
+    if (a*c>b*d){
+        g=a*c;
     }
-    else
-    {
-    g=b*d;
+    else{
+        g=b*d;
     }
-    if (g>a*d)
-    {
-    e=g;
+    
+    if (g>a*d){
+        e=g;
     }
-    else
-    {
-    e=a*d;
+    else{
+        e=a*d;
     }
-    if (e>b*c)
-    {
-    t=e;
+
+    if (e>b*c){
+        t=e;
     }
-    else
-    {
-    t=b*c;
+    else{
+        t=b*c;
     }
+
     printf("%lld", t);
     return 0;
 }
