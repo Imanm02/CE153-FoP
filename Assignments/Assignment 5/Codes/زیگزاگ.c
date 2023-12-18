13eaf847431852f1ce725956d6e90aa4ed95ff81
Imanm02 <imanm1381@gmail.com>
2023-12-18 15:40:24 +0330

    Refactor Assignment5 codes

--- Assignments/Assignment 5/Codes/زیگزاگ.c
+++ Assignments/Assignment 5/Codes/زیگزاگ.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int i, a;
-    scanf("%d", &i);
-    int bang[i][i];
-    for (int x = 0; x < i; x++){
-        for (int y = 0; y < i; y++){
-            scanf("%d", &a);
-            bang[x][y] = a ;
+int main() {
+    int size;
+    scanf("%d", &size);
+    int matrix[size][size];
+
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
+            scanf("%d", &matrix[row][col]);
         }
     }
-    for (int z = 0; z <= (2 * i) - 2; z++){
-        if (z%2 == 0){
-            for (int x = i - 1; x > -1; x--){
-                for (int y = 0; y <= i - 1; y++){
-                    if (x+y == z) {printf("%d ", bang[x][y]);}
+
+    for (int sum = 0; sum <= (2 * size) - 2; sum++) {
+        if (sum % 2 == 0) {
+            for (int x = size - 1; x > -1; x--) {
+                for (int y = 0; y <= size - 1; y++) {
+                    if (x + y == sum) printf("%d ", matrix[x][y]);
+                }
+            }
+        } else {
+            for (int x = 0; x <= size - 1; x++) {
+                for (int y = size - 1; y > -1; y--) {
+                    if (x + y == sum) printf("%d ", matrix[x][y]);
                 }
             }
         }
-        else if (z%2 == 1){
-            for (int x = 0; x <= i - 1; x++){
-                for (int y = i - 1; y > -1; y--){
-                    if (x+y == z) {printf("%d ", bang[x][y]);}
-                }
-            }
-        }}
+    }
     return 0;
 }
