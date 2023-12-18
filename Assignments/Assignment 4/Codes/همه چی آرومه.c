a83a0f130058e177c477cf46e88792ebe91932c4
Imanm02 <imanm1381@gmail.com>
2023-12-18 15:36:48 +0330

    Refactor Assignment4 codes

--- Assignments/Assignment 4/Codes/همه چی آرومه.c
+++ Assignments/Assignment 4/Codes/همه چی آرومه.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-double tabe(double n, double a, double b, double c, double d, double e, double f)
-{
-    return (a*n*n*n*n*n + b*n*n*n*n + c*n*n*n + d*n*n + e*n +f);
+
+double polynomialFunction(double n, double a, double b, double c, double d, double e, double f) {
+    return a * n * n * n * n * n + b * n * n * n * n + c * n * n * n + d * n * n + e * n + f;
 }
-double bazgashti(double n, double m, double a, double b, double c, double d, double e, double f)
-{
-    if (tabe((m+n)/2, a, b, c, d, e, f) == 0 ) {return (m+n)/2;}
-    else if (tabe((m+n)/2, a, b, c, d, e, f)*tabe(n, a, b, c, d, e, f)<0) {
-        return bazgashti(n, (m+n)/2, a, b, c, d, e, f);
-    }
-    else {
-        return bazgashti(m, (m+n)/2, a, b, c, d, e, f);
-    }
+
+double findRoot(double left, double right, double a, double b, double c, double d, double e, double f) {
+    double mid = (left + right) / 2;
+    double midValue = polynomialFunction(mid, a, b, c, d, e, f);
+
+    if (midValue == 0) return mid;
+    else if (midValue * polynomialFunction(left, a, b, c, d, e, f) < 0) 
+        return findRoot(left, mid, a, b, c, d, e, f);
+    else 
+        return findRoot(mid, right, a, b, c, d, e, f);
 }
-int main()
-{
-    double a, b, c, d, e, f, m, n;
-    scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &e, &f, &m, &n);
-    if ( tabe(m, a, b, c, d, e, f) == 0 ) { printf("%f", m);}
-    else if (tabe(n, a, b, c, d, e, f)==0) {printf("%f", n);}
-    else if (tabe(m, a, b, c, d, e, f) * tabe(n, a, b, c, d, e, f)>0) {
+
+int main() {
+    double a, b, c, d, e, f, left, right;
+    scanf("%lf%lf%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &e, &f, &left, &right);
+
+    if (polynomialFunction(left, a, b, c, d, e, f) == 0) printf("%f", left);
+    else if (polynomialFunction(right, a, b, c, d, e, f) == 0) printf("%f", right);
+    else if (polynomialFunction(left, a, b, c, d, e, f) * polynomialFunction(right, a, b, c, d, e, f) > 0)
         printf("NOT POSSIBLE!");
-    }
-    else {printf("%f", bazgashti(n, m, a, b, c, d, e, f));}
+    else printf("%f", findRoot(right, left, a, b, c, d, e, f));
+
     return 0;
 }
