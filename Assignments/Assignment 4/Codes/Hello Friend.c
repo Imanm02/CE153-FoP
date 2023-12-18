a02ae3d5fa52510ad14fafcedef58570d23d6b37
Imanm02 <imanm1381@gmail.com>
2023-12-18 15:35:27 +0330

    Refactor Assignment4 codes

--- Assignments/Assignment 4/Codes/Hello Friend.c
+++ Assignments/Assignment 4/Codes/Hello Friend.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-int tabe1(int n)
-{
-    if (n==0) {return 0;}
-    else if (n==1) {return 1;}
-    else {return (tabe1(n/2) + tabe1(n/3) + n*n);}
+
+int computeFirstFunction(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return computeFirstFunction(n / 2) + computeFirstFunction(n / 3) + n * n;
 }
-int tabe2(int n)
-{
-    if (n==0) {return 0;}
-    else if (n==1) {return 1;}
-    else {return (tabe2(n/2) + tabe2(n/3) + 2*n*n);}
+
+int computeSecondFunction(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return computeSecondFunction(n / 2) + computeSecondFunction(n / 3) + 2 * n * n;
 }
-int main()
-{
-    int a;
-    scanf("%d", &a);
-    if (a<500 || a==0 || a==1) {printf("%d", tabe1(a));}
-    else if (a>=500) {printf("%d", tabe2(a));}
+
+int main() {
+    int number;
+    scanf("%d", &number);
+    if (number < 500 || number == 0 || number == 1) printf("%d", computeFirstFunction(number));
+    else if (number >= 500) printf("%d", computeSecondFunction(number));
     return 0;
 }
--- Assignments/Assignment 4/Codes/حرف زدن بسه.c
+++ Assignments/Assignment 4/Codes/حرف زدن بسه.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void Shekl(int n)
-{
-    for(int i=1; i<=2*n; i++){
-        for(int j=1; j<=2*n; j++){
-            if (i==1 || j==2*n - i + 1){
-                printf("*");
-            }
-            else {
-                printf(" ");
-            }
+
+void printPattern(int n) {
+    for (int i = 1; i <= 2 * n; i++) {
+        for (int j = 1; j <= 2 * n; j++) {
+            if (i == 1 || j == 2 * n - i + 1) printf("*");
+            else printf(" ");
         }
         printf("\n");
     }
 }
-void tekrar(int n)
-{
-    if (n > 0){
-        Shekl(n);
-        tekrar(n-1);
+
+void repeatPattern(int n) {
+    if (n > 0) {
+        printPattern(n);
+        repeatPattern(n - 1);
     }
 }
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    tekrar(n);
+
+int main() {
+    int size;
+    scanf("%d", &size);
+    repeatPattern(size);
     return 0;
 }
