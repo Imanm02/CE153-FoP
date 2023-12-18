6f0468b6ac916baf34bea2a916826847d6d9eaeb
Imanm02 <imanm1381@gmail.com>
2023-12-18 15:26:39 +0330

    Clean code Assignment3 codes

--- Assignments/Assignment 3/Codes/مسافرخانه‌ی جَک زرد.c
+++ Assignments/Assignment 3/Codes/مسافرخانه‌ی جَک زرد.c
@@ -1,53 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int maximum(int a, int b, int c)
-{
-    if (a >= b && a >= c) {return a;}
-    else if (b >= a && b >= c) {return b;}
-    else {return c;}
-}
-int minimum(int a, int b, int c)
-{
-    if (a <= b && a <= c) {return a;}
-    else if (b <= a && b <= c) {return b;}
-    else {return c;}
-}
-int medium(int a, int b, int c)
-{
-    if (a <= b && a >= c) {return a;}
-    if (a <= c && a >= b) {return a;}
-    else if (b <= a && b >= c) {return b;}
-    else if (b <= c && b >= a) {return b;}
-    else {return c;}
+int findMaximum(int num1, int num2, int num3) {
+    if (num1 >= num2 && num1 >= num3) return num1;
+    else if (num2 >= num1 && num2 >= num3) return num2;
+    else return num3;
 }
 
+int findMinimum(int num1, int num2, int num3) {
+    if (num1 <= num2 && num1 <= num3) return num1;
+    else if (num2 <= num1 && num2 <= num3) return num2;
+    else return num3;
+}
 
-int main()
-{
-    float a1, a2, a3;
-    int  point1, point2, point3, Rmin, Rmax, Rmed, n, i, j;
-    int num1=0;
-    scanf("%f%*s %f%*s %f%*s", &a1, &a2, &a3);
-    if (a1==a2 || a1==a3 || a2==a3) {printf("error");}
-    else {
-        scanf("%d %d %d", &point1, &point2, &point3);
-        scanf("%d", &Rmin);
-        Rmax = Rmin * maximum(a1, a2, a3) / minimum(a1, a2, a3);
-        Rmed = Rmin * medium(a1, a2, a3) / minimum(a1, a2, a3);
-        scanf("%d", &n);
-        for (int o=0; o<n; o++) {
-            scanf("%d %d", &i, &j);
-            if ((i*i + j*j) <= (Rmin * Rmin)) {
-                num1 += maximum(point1, point2, point3);}
-            else if ((i*i + j*j) <= (Rmed * Rmed)) {
-                num1 += medium(point1, point2, point3);}
-            else if ((i*i + j*j) <= (Rmax * Rmax)) {
-                num1 += minimum(point1, point2, point3);}
+int findMedium(int num1, int num2, int num3) {
+    if ((num1 <= num2 && num1 >= num3) || (num1 <= num3 && num1 >= num2)) return num1;
+    else if ((num2 <= num1 && num2 >= num3) || (num2 <= num3 && num2 >= num1)) return num2;
+    else return num3;
+}
+
+int main() {
+    float ratio1, ratio2, ratio3;
+    int score1, score2, score3, minRadius, maxRadius, medRadius, numThrows, totalScore = 0;
+    scanf("%f %f %f", &ratio1, &ratio2, &ratio3);
+
+    if (ratio1 == ratio2 || ratio1 == ratio3 || ratio2 == ratio3) {
+        printf("error");
+    } else {
+        scanf("%d %d %d", &score1, &score2, &score3);
+        scanf("%d", &minRadius);
+
+        maxRadius = minRadius * findMaximum(ratio1, ratio2, ratio3) / findMinimum(ratio1, ratio2, ratio3);
+        medRadius = minRadius * findMedium(ratio1, ratio2, ratio3) / findMinimum(ratio1, ratio2, ratio3);
+
+        scanf("%d", &numThrows);
+        for (int i = 0; i < numThrows; i++) {
+            int x, y;
+            scanf("%d %d", &x, &y);
+            int distanceSquared = x * x + y * y;
+
+            if (distanceSquared <= minRadius * minRadius) {
+                totalScore += findMaximum(score1, score2, score3);
+            } else if (distanceSquared <= medRadius * medRadius) {
+                totalScore += findMedium(score1, score2, score3);
+            } else if (distanceSquared <= maxRadius * maxRadius) {
+                totalScore += findMinimum(score1, score2, score3);
             }
-        printf("%d", num1);
+        }
+        printf("%d", totalScore);
     }
     return 0;
 }
-
